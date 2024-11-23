82cfbb8d806a0bcecf9c06b65d3f79973374d15f
Nour <nshbair@gmail.com>
2024-11-23 14:13:11 +0000

    Fix bug in indentation by adding tab space to comment

--- quiz_app/model/user.py
+++ quiz_app/model/user.py
@@ -1,10 +1,10 @@
 
 class User:
-"""
-This class for User object,
-and it contains the user data which are:
-username: str, question_number: int, points: int
-"""
+    """
+    This class for User object,
+    and it contains the user data which are:
+    username: str, question_number: int, points: int
+    """
     def __init__(self, username, question_number, points):
         self.username = username
         self.question_number = int(question_number)
